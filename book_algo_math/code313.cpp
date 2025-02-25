25c56f693554dbe2bd5f45957e9c8a73a6582729
cpa-tomoe-DS <been.enimagan@gmail.com>
2025-02-25 12:06:40 +0900

    Nの約数を全て出力する_2回目

--- book_algo_math/code313.cpp
+++ book_algo_math/code313.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
 
+
+/** Nの約数をすべて出力するプログラム */ 
 int main() {
     long long N;
     cin >> N;
 
     for (long long i = 1; i * i <= N; i++) {
         if (N % i == 0) {
-            cout << i << N / i << endl; 
+            cout << i << " ";  // スペースを入れる
+            if (i != N/i) {    // 平方数の重複を防ぐ
+                cout << N/i << " ";
+            }
         }
     }
+    cout << endl;  // 最後に改行
     return 0;
 }
